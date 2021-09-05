f17f0398af72ad4f4c7c268db141dc3db6ce1db4
rzela <zelayasama@gmail.com>
2021-09-05 12:48:51 -0600

    ui mockup

--- src/App.js
+++ src/App.js
@@ -2,28 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="success">TEST</Button>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* SEACHER */}
+			<InputGroup className="mb-3">
+				<InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+				<FormControl
+					placeholder="Username"
+					aria-label="Username"
+					aria-describedby="basic-addon1"
+				/>
+			</InputGroup>
+
+			{/* TODOS GROUP PENDING */}
+			<div>
+				<p> Pending [NUMBER OF TODOS]</p>
+				<ul>
+					<li>TODO 1</li>
+					<li>TODO 2</li>
+					<li>TODO 3</li>
+				</ul>
+			</div>
+
+			{/* TODOS GROUP COMPLETED */}
+			<div>
+				<p> Completed [NUMBER OF TODOS]</p>
+				<ul>
+					<li>TODO 1</li>
+					<li>TODO 2</li>
+					<li>TODO 3</li>
+				</ul>
+			</div>
+
+			{/* CREATE BUTTON */}
+			<Button variant="success">+</Button>
+		</div>
+	);
 }
 
 export default App;
